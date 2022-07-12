289d77b4fd75c62851dd989748a94f7763464b4e
unknown <johnloydcorletadobas@gmail.com>
2022-07-12 17:49:13 +0800

    added main content and styles

---
+++ scripts/workContent.js
@@ -0,0 +1,58 @@
+
+    document.getElementsByClassName("experience")[0].addEventListener("click", changeContent);
+
+    function changeContent() {
+
+        var content =   "<div class=\"content-sections border\">" +
+                            "<header class=\"line-height\">" +
+                                "Software Development Intern (100 hrs.) <br>" +
+                                "<span>" +
+                                    "Datatrends Co. <br>" +
+                                    "Quezon City" +
+                                "</span>" + 
+                            "</header>" +
+                            "<p> Automation of gathering data based on their categories. " +
+                                "It records the tabulated data from a csv file, and stores" +
+                                "the information to a database. </p>" +
+                        "</div>" +
+
+                        "<div class=\"content-sections border\">" +
+                            "<header class=\"line-height\">" +
+                                "Web Development Intern: (300 hrs.) <br>" +
+                                "<span>" +
+                                    "Datatrends Co. <br>" +
+                                    "Quezon City" +
+                                "</span>" + 
+                            "</header>" +
+                            "<p> Creation of a website for payment processing platform. Content " +
+                                "Management System (CMS) was used, particularly Wordpress. </p>" +
+                        "</div>" + 
+
+                        "<div class=\"content-sections border\">" +
+                            "<header class=\"line-height\">" +
+                                "Mobile App Dev Intern: March - Present <br>" +
+                                "<span>" +
+                                    "SMOL Budgeting App <br>" +
+                                    "Quezon City" +
+                                "</span>" + 
+                            "</header>" +
+                            "<p> Creation of a budgeting application, to help users  in handling their money." + 
+                                "It gives overview, feedback, graphical representation, reports, and transactions" + 
+                                "of their spending. React Native is the framework used." + 
+                        "</div>" +
+
+                        "<div class=\"content-sections border\">" +
+                            "<header class=\"line-height\">" +
+                                "Mobile App Dev Intern: March - Present <br>" +
+                                "<span>" +
+                                    "SMOL Budgeting App <br>" +
+                                    "Quezon City" +
+                                "</span>" + 
+                            "</header>" +
+                            "<p> Creation of a budgeting application, to help users  in handling their money." + 
+                                "It gives overview, feedback, graphical representation, reports, and transactions" + 
+                                "of their spending. React Native is the framework used." + 
+                        "</div>";
+                        
+        document.getElementsByClassName("content")[0].innerHTML = content;
+    }
