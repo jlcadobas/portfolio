2d5480de0c24b2ec168a4e10b7f1f404be74d0ba
unknown <johnloydcorletadobas@gmail.com>
2023-01-19 11:28:15 +0800

    added new web designs

--- scripts/portfolio.js
+++ scripts/portfolio.js
@@ -25,9 +25,23 @@
                         "</div>" +
 
                         "<div class=\"content-sections\">" +
-                            "<header> Art Town </header>" +
-                            "<a href=\"https://github.com/mnslnln/ART-TOWN\" target=\"_blank\">" +
-                                "<img class=\"featured-website\" src=\"./images/Art Town.png\"/>" +
+                            "<header> Write A Story </header>" +
+                            "<a href=\"https://www.figma.com/file/0Co1H0RHQxtCXluNXOEHWi/Write-A-Story?node-id=0%3A1&t=Pr3yUQt7FdhOdPQA-1\" target=\"_blank\">" +
+                                "<img class=\"featured-website\" src=\"./images/Write-A-Story.PNG\"/>" +
+                            "</a>" +
+                        "</div>" +
+
+                        "<div class=\"content-sections\">" +
+                            "<header> Juan Dela Cook </header>" +
+                            "<a href=\"https://www.figma.com/file/7oQjXXEyK5QQjeDASIT7Tm/Juan-Dela-Cook\" target=\"_blank\">" +
+                                "<img class=\"featured-website\" src=\"./images/Juan-Dela-Cook.PNG\"/>" +
+                            "</a>" +
+                        "</div>" +
+
+                        "<div class=\"content-sections\">" +
+                            "<header> Melodify </header>" +
+                            "<a href=\"https://www.figma.com/file/4xlbKLLXFW0r3llWo0hFsc/Melodify?node-id=0%3A1&t=dqSbdgIWNjJZsFx6-1\" target=\"_blank\">" +
+                                "<img class=\"featured-website\" src=\"./images/Melodify.jpg\"/>" +
                             "</a>" +
                         "</div>" +
 
@@ -40,22 +54,15 @@
 
                         "<div class=\"content-sections\">" +
                             "<header> SMOL Budgeting App </header>" +
-                            "<a href=\"https://github.com/jlcadobas/SMOL\" target=\"_blank\">" +
+                            "<a href=\"https://www.figma.com/file/FNNtCgJW26zXAzCioX8wxz/SMOL-Budgeting-App?node-id=54%3A459&t=gTrl9pEHj4b9bwMv-1\" target=\"_blank\">" +
                                 "<img class=\"featured-website\" src=\"./images/smol.png\"/>" +
                             "</a>" +
                         "</div>" +
 
                         "<div class=\"content-sections\">" +
-                            "<header> Juan Dela Cook </header>" +
-                            "<a href=\"https://www.figma.com/file/7oQjXXEyK5QQjeDASIT7Tm/Juan-Dela-Cook\" target=\"_blank\">" +
-                                "<img class=\"featured-website\" src=\"./images/Juan-Dela-Cook.PNG\"/>" +
-                            "</a>" +
-                        "</div>" +
-
-                        "<div class=\"content-sections\">" +
-                            "<header> Write A Story </header>" +
-                            "<a href=\"https://www.figma.com/proto/0Co1H0RHQxtCXluNXOEHWi/Untitled?node-id=0%3A1\" target=\"_blank\">" +
-                                "<img class=\"featured-website\" src=\"./images/Write-A-Story.PNG\"/>" +
+                            "<header> AppointPet </header>" +
+                            "<a href=\"https://www.figma.com/file/8EXyLdjDXdXq7FAF5or7a8/AppointPet?node-id=0%3A1&t=7ZnEWYsLNHWfxUI0-1\" target=\"_blank\">" +
+                                "<img class=\"featured-website\" src=\"./images/AppointPet.PNG\"/>" +
                             "</a>" +
                         "</div>";
     
