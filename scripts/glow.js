c030767c15fd8cc4a2711f12b8e6f228c94b45d1
John Loyd Adobas <johnloydcorletadobas@gmail.com>
2025-07-03 17:56:42 +0800

    fix glow and link effects

--- scripts/glow.js
+++ scripts/glow.js
@@ -1,10 +1,24 @@
 
     document.addEventListener('mousemove', function(e) {
-    const tracker = document.getElementById('tracker');
-    const rect = tracker.getBoundingClientRect();
-    const offsetX = rect.width / 2;
-    const offsetY = rect.height / 2;
+        // Get necessary element for #tracker div for the glow effect
+        const tracker = document.getElementById('tracker');
+        const rect = tracker.getBoundingClientRect();
+        const offsetX = rect.width / 2;
+        const offsetY = rect.height / 2;
 
-    tracker.style.left = (e.clientX - offsetX) + 'px';
-    tracker.style.top = (e.clientY - offsetY) + 'px';
+        // Get necessary element for adding classes for the navigation links (About, Experience and Projects)
+        const links = document.querySelectorAll('.link-container a');
+
+        links.forEach(link => {
+            link.addEventListener('click', (e) => {
+                // Remove 'active' from all links
+                links.forEach(l => l.classList.remove('active'));
+
+                // Add 'active' to the clicked one
+                link.classList.add('active');
+            });
+        });
+
+        tracker.style.left = (e.clientX - offsetX) + 'px';
+        tracker.style.top = (e.clientY - offsetY) + 'px';
     });
